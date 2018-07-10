26571a3160d166f7ab5a97901c5fcf4840fa259c
wue19 <wue19@bcdschool.org>
2018-07-10 15:54:03 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -16,8 +16,12 @@ puts snacks("Swedish fish")
 ## This returns "Any time, is the right time for Sweedish fish!"
 
 # call your methods here
-
+puts breakfast("bacon")
+puts lunch("turkey sandwich")
+puts dinner("steak")
 
 # Call your methods without any arguments here
 puts snacks
-## In our snacks method, the default choice is "Cheetos" Calling snacks like this without specifying a choice will return "Any time, is the right time for Cheetos!"
+puts breakfast
+puts lunch
+puts dinner
