0a4ffaee504eec97eb02c727f373534f596491fe
wue19 <wue19@bcdschool.org>
2018-07-10 15:56:04 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -3,8 +3,7 @@ def snacks(food="Cheetos")
   "Any time, is the right time for #{food}!"
 end
 
-# Define breakfast, lunch and dinner methods that return the meal choice passed into them. If nothing is passed in, it shoud default to the foods on the readme (frosted flake, grilled cheese, salmon)
-
+# Define breakfast, lunch and dinner methods that return the meal choice passed into them with a default to your favorite.
 def breakfast(food="Frosted Flakes")
   "Morning is the best time for #{food}"
 end
@@ -21,7 +20,7 @@ end
 
 # Call the methods with puts and your own arguments here. Like this:
 puts snacks("Swedish fish")
-## This returns "Any time, is the right time for Sweedish fish!"
+
 
 # call your methods here
 puts breakfast("Bacon")
